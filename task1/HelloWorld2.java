b27789bc5b490309db443c4781763071bd51a250
andrey.yermilov@gmail.com <alhimik1988>
2016-06-30 14:42:42 +0300

    jar commit

--- task1/HelloWorld2.java
+++ task1/HelloWorld2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HelloWorld2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Как тебя зовут?");
+        System.out.println("Как твоё имя?");
         String name = sc.nextLine();
         System.out.println("Привет, "+ name);
     }
